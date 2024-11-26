9ef3cd486c4d6e6d65346b10e67512e5b8986964
Gloria Mokberi <gloriam@axis.com>
2024-11-26 19:41:27 +0400

    finished

--- src/test/java/etsn20_lab1/NextDateTest.java
+++ src/test/java/etsn20_lab1/NextDateTest.java
@@ -48,4 +48,43 @@ public class NextDateTest {
 		assertEquals("2/6/2006", nd.run(2, 5, 2006));
 	}
 	
+	@Test
+	public void testDayLessThan31InDec() {
+		assertEquals("12/6/2006", nd.run(12, 5, 2006));
+	}
+	
+	@Test
+	public void testDayMoreThan31InDecAndYear2021() {
+		assertEquals("Invalid Next Year", nd.run(12, 32, 2021));
+	}
+	
+	@Test
+	public void testDayMoreThan31InDecAndYearNot2021() {
+		assertEquals("1/1/2020", nd.run(12, 32, 2019));
+	}
+	
+	@Test
+	public void testIfDayLessThan30() {
+		assertEquals("4/16/2006", nd.run(4, 15, 2006));
+	}
+	
+	@Test
+	public void testIfDay30() {
+		assertEquals("5/1/2006", nd.run(4, 30, 2006));
+	}
+	
+	@Test
+	public void testIfDayMoreThan30() {
+		assertEquals("Invalid Input Date", nd.run(4, 31, 2006));
+	}
+	
+	@Test
+	public void testIfDayLessThan31() {
+		assertEquals("3/22/2006", nd.run(3, 21, 2006));
+	}
+	
+	@Test
+	public void testIfDayMoreThan31() {
+		assertEquals("4/1/2006", nd.run(3, 32, 2006));
+	}
 }
